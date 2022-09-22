b48cb77dd022a412a22026e9da3a8abdedebcbc0
Wigu701 <72235998+Wigu701@users.noreply.github.com>
2022-09-21 20:47:28 -0500

    Updated LED pin for clarity

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <mbed.h>
 
-DigitalOut led1(LED1);
+DigitalOut led1(D13);
 
+// Simulates motor ramping up, maintaining, and then slowing down
 void simulateMotor() {
   int pulsePerSec = 1;
   led1 = 0;
   int usTime;
-  // Ramp up
+
+  // Ramp up from 1/48rps to 1rps
   for (;pulsePerSec < 49; pulsePerSec++) {
       usTime = 1000000 / pulsePerSec;
       for (int i = 0; i < 10; i++) {
@@ -14,13 +16,14 @@ void simulateMotor() {
           wait_us(usTime);
       }
   }
+
   // Run at 1rps
   for (int i = 0; i < 480; i++) {
       led1 = !led1;
       wait_us(usTime);
   }
 
-  // Ramp down
+  // Ramp down to 1/48rps
   for (;pulsePerSec > 0; pulsePerSec--) {
       usTime = 1000000 / pulsePerSec;
       for (int i = 0; i < 10; i++) {
@@ -34,7 +37,6 @@ void simulateMotor() {
 
 int main() {
   
-  // What you do in the main loop is not important
   while(1) {
       simulateMotor();
       wait_us(3000000);
