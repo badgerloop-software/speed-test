02720578bae3584fa841e0b368e9380909499bf7
Wigu701 <72235998+Wigu701@users.noreply.github.com>
2022-09-21 20:43:53 -0500

    Implemented function simulating motor pulse

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
-#include "mbed.h"
+#include <mbed.h>
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+DigitalOut led1(LED1);
 
-    }
+void simulateMotor() {
+  int pulsePerSec = 1;
+  led1 = 0;
+  int usTime;
+  // Ramp up
+  for (;pulsePerSec < 49; pulsePerSec++) {
+      usTime = 1000000 / pulsePerSec;
+      for (int i = 0; i < 10; i++) {
+          led1 = !led1;
+          wait_us(usTime);
+      }
+  }
+  // Run at 1rps
+  for (int i = 0; i < 480; i++) {
+      led1 = !led1;
+      wait_us(usTime);
+  }
+
+  // Ramp down
+  for (;pulsePerSec > 0; pulsePerSec--) {
+      usTime = 1000000 / pulsePerSec;
+      for (int i = 0; i < 10; i++) {
+          led1 = !led1;
+          wait_us(usTime);
+      }
+  }
+  led1 = 1;
 }
 
+
+int main() {
+  
+  // What you do in the main loop is not important
+  while(1) {
+      simulateMotor();
+      wait_us(3000000);
+  }
+}
