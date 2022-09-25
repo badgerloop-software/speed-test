e3f3d74fb861709a2de6088522b5c6e2323694ac
Wigu701 <72235998+Wigu701@users.noreply.github.com>
2022-09-25 11:26:40 -0500

    Added hold rpm function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <mbed.h>
 
+// 0 to hold speed, 1 to simulate motor
+#define HOLD_OR_MOTOR 0
+#define HOLD_SPEED 11
+#define MOTOR_START_SPEED 1
+#define MOTOR_TOP_SPEED 55
+#define RUN_DELAY 3
+
+
 DigitalOut led1(D13);
+
+void holdRps(int rps) {
+    int usTime = 500000 / rps;
+    for (int i = 0; i < 112; i++) {
+      led1 = !led1;
+      wait_us(usTime);
+    }
+}
+
 
 // Simulates motor ramping up, maintaining, and then slowing down
 void simulateMotor() {
@@ -8,6 +25,7 @@ void simulateMotor() {
   led1 = 0;
   int usTime;
 
+  /*
   printf("Starting\n");
   // Ramp up from 1/48rps to 1rps
   for (;pulsePerSec < 3872; pulsePerSec += 1) {
@@ -22,13 +40,16 @@ void simulateMotor() {
       
   }
   printf("At top speed\n");
-
+  */
+  
+  usTime = 1000000 / 22;
   // Run at 1rps
-  for (int i = 0; i < 19360; i++) {
+  for (int i = 0; i < 112; i++) {
       led1 = !led1;
       wait_us(usTime);
   }
-
+  
+  /*
   printf("Ramping down\n");
   // Ramp down to 1/48rps
   for (;pulsePerSec > 48; pulsePerSec -= 1) {
@@ -43,13 +64,14 @@ void simulateMotor() {
   }
   led1 = 0;
   printf("Done\n");
+  */
 }
 
 
 int main() {
   
   while(1) {
-      simulateMotor();
+      holdRps(11);
       wait_us(3000000);
   }
 }
