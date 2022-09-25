1f9e50314776cb84341edcf192c60d7c36a662b5
Wigu701 <72235998+Wigu701@users.noreply.github.com>
2022-09-25 11:11:27 -0500

    Altered code to ramp to 41rps

--- main.cpp
+++ main.cpp
@@ -4,34 +4,45 @@ DigitalOut led1(D13);
 
 // Simulates motor ramping up, maintaining, and then slowing down
 void simulateMotor() {
-  int pulsePerSec = 1;
+  int pulsePerSec = 48;
   led1 = 0;
   int usTime;
 
+  printf("Starting\n");
   // Ramp up from 1/48rps to 1rps
-  for (;pulsePerSec < 49; pulsePerSec++) {
-      usTime = 1000000 / pulsePerSec;
-      for (int i = 0; i < 10; i++) {
-          led1 = !led1;
-          wait_us(usTime);
+  for (;pulsePerSec < 3872; pulsePerSec += 1) {
+      if (!(pulsePerSec & 0x5)) {
+        printf("Current rps %d\n", pulsePerSec / 48); 
       }
+      for (int i = 0; i < 10; i++) {
+        led1 = !led1;
+        wait_us(usTime);
+      }
+      usTime = 1000000 / pulsePerSec;
+      
   }
+  printf("At top speed\n");
 
   // Run at 1rps
-  for (int i = 0; i < 480; i++) {
+  for (int i = 0; i < 19360; i++) {
       led1 = !led1;
       wait_us(usTime);
   }
 
+  printf("Ramping down\n");
   // Ramp down to 1/48rps
-  for (;pulsePerSec > 0; pulsePerSec--) {
-      usTime = 1000000 / pulsePerSec;
-      for (int i = 0; i < 10; i++) {
-          led1 = !led1;
-          wait_us(usTime);
+  for (;pulsePerSec > 48; pulsePerSec -= 1) {
+      if (!(pulsePerSec & 0x5)) {
+        printf("Current rps %d\n", pulsePerSec / 48); 
       }
+      for (int i = 0; i < 10; i++) {
+        led1 = !led1;
+        wait_us(usTime);
+      }
+      usTime = 1000000 / pulsePerSec; 
   }
-  led1 = 1;
+  led1 = 0;
+  printf("Done\n");
 }
 
 
