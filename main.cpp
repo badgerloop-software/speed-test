b78b0ad8de5ff70210f899fd2f3e93f23ea60b45
bquan0 <powerbowen@gmail.com>
2022-09-25 11:45:45 -0500

    ticker program to calculate rps

--- main.cpp
+++ main.cpp
@@ -3,41 +3,46 @@ of a rotation. The program waits until 48 interrupts have occurred (one full
 rotation) to calculate rps but could be edited to caluclate on an arbitrary cycle
 */
 #include "mbed.h"
+#define WAIT_TIME 100ms
+/**
+How to calculate WAIT_TIME based on diameter of wheel
+mph * 5280 * 12 / 3600 / pi / diameter = rps (rotations per second)
+rps * 48 = ticks per second
+(ticks per second)^-1 = seconds per tick = WAIT_TIME
+*/
 
-InterruptIn button(LED1);
-int counter = 0;
 
-int incrementCounter()
+InterruptIn button(LED1); // the LED which is modeling the interrupt from the motor
+Ticker reader; // the timer interrupt that causes rps to be calculated
+static int counter = 0; // number of ticks, increments with every interrupt from the LED(motor)
+static float rps = 0; // rotations per second of the wheel
+
+void incrementCounter()
 {
-counter++;
-return 0;
+    counter++;
 }
 
 int resetCounter() 
 {
-counter = 0;
-return 0;
+    counter = 0;
+    return 0;
 }
 
+void readCounter()
+{
+    rps = (counter/48.0)/WAIT_TIME.count()*1000.0;
+    resetCounter();
+}
 
 int main()
 {
+    // tell the interrupts which functions to run when they fire
     button.rise(&incrementCounter);
-    time_t start = time(NULL);
+    reader.attach(&readCounter, WAIT_TIME);
     printf("starting!\n");
     while(1) {
-       if (counter == 48) {   //one roto has occurred
-       //rotations per second calculation:
-           time_t end = time(NULL);
-           time_t total = (end - start);
-           //do something to output this number, I don't know what
-            printf("It took %d units\n", total);
-           //prepare for new rps calculation
-           if (resetCounter()) {
-               printf("Error occurred while resetting counter");
-           }
-           start = time(NULL);
-       }
+        // print out the rps every second
+        printf("The rps is: %f\n", rps);
+        wait_us(1000000);
     }
-    
 }
